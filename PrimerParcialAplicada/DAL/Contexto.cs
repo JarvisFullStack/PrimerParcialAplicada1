69e1f5be8a456fd5afe11ac6e415ec0e60cf85c8
JarvisXxX <eldrizzy@hotmail.com>
2018-09-17 13:33:19 -0600

    Estructura basica | Primer Parcial

---
+++ PrimerParcialAplicada/DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+echo "# PrimerParcialAplicada1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/JarvisFullStack/PrimerParcialAplicada1.git
+git push -u origin master 
+*/
+namespace PrimerParcialAplicada.DAL
+{
+    public class Contexto : DbContext
+    {
+        //public DbSet<Tipo> Tipo { get; set; };
+        public Contexto() : base("ConStr") { }
+
+    }
+}
---
+++ PrimerParcialAplicada/UI/Registros/rRegistro.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace PrimerParcialAplicada.UI.Registros
+{
+    partial class rRegistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mantenimientobutton = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuargar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Mantenimientobutton
+            // 
+            this.Mantenimientobutton.Location = new System.Drawing.Point(256, 132);
+            this.Mantenimientobutton.Name = "Mantenimientobutton";
+            this.Mantenimientobutton.Size = new System.Drawing.Size(75, 23);
+            this.Mantenimientobutton.TabIndex = 49;
+            this.Mantenimientobutton.Text = "M";
+            this.Mantenimientobutton.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuPopup;
+            this.btnBuscar.BackColor = System.Drawing.Color.Transparent;
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Image = global::PrimerParcialAplicada.Properties.Resources.find;
+            this.btnBuscar.Location = new System.Drawing.Point(304, 10);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(72, 52);
+            this.btnBuscar.TabIndex = 40;
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnGuargar);
+            this.groupBox1.Controls.Add(this.btnNuevo);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(3, 189);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 66);
+            this.groupBox1.TabIndex = 39;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Transparent;
+            this.btnEliminar.FlatAppearance.BorderSize = 0;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Image = global::PrimerParcialAplicada.Properties.Resources.delete;
+            this.btnEliminar.Location = new System.Drawing.Point(277, 6);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 60);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btnGuargar
+            // 
+            this.btnGuargar.BackColor = System.Drawing.Color.Transparent;
+            this.btnGuargar.FlatAppearance.BorderSize = 0;
+            this.btnGuargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuargar.Image = global::PrimerParcialAplicada.Properties.Resources.save;
+            this.btnGuargar.Location = new System.Drawing.Point(148, 6);
+            this.btnGuargar.Name = "btnGuargar";
+            this.btnGuargar.Size = new System.Drawing.Size(75, 60);
+            this.btnGuargar.TabIndex = 1;
+            this.btnGuargar.UseVisualStyleBackColor = false;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.Color.Transparent;
+            this.btnNuevo.FlatAppearance.BorderSize = 0;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.Image = global::PrimerParcialAplicada.Properties.Resources._new;
+            this.btnNuevo.Location = new System.Drawing.Point(12, 6);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 60);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // rRegistro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 336);
+            this.Controls.Add(this.Mantenimientobutton);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "rRegistro";
+            this.Text = "rRegistro";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Mantenimientobutton;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGuargar;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
